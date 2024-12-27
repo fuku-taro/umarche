2c89b109e7b271ab664e9e0c0cff291bbee931c9
fuku-taro <fukumitsu.seko@gmail.com>
2024-12-27 12:59:15 +0900

    クラスのマージ

--- src/resources/views/components/tests/card.blade.php
+++ src/resources/views/components/tests/card.blade.php
@@ -1,4 +1,13 @@
-<div class="border-2 shadow-md w-1/4 p-2">
+@props([
+  'title' => '',
+  'content' => '初期値',
+  'message' => '初期値',
+  ])
+
+<div {{ $attributes->merge([
+  'class' => 'border-2 shadow-md p-2'
+]) }} >
+{{-- <div {{ $attributes }} class="border-2 shadow-md p-2" > --}}
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
--- src/resources/views/tests/component-test1.blade.php
+++ src/resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
   <x-slot name="header">ヘッダー１</x-slot>
   コンポーネント１
   <x-tests.card title="タイトル" content="本文" :message="$message"/>
+  <x-tests.card title="CSS" class="bg-red-300"/>
 </x-tests.app>
