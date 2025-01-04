1c70ef949d34da014658d34feb7eca1c81549eff
fuku-taro <fukumitsu.seko@gmail.com>
2025-01-04 15:49:53 +0900

    imageSeeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -26,7 +26,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
-            ShopSeeder::class
+            ShopSeeder::class,
+            ImageSeeder::class
         ]);
     }
 }
