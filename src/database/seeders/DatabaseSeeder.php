9219bc05e8e286dbad5b0f8ff09dad3438b54312
fuku-taro <fukumitsu.seko@gmail.com>
2025-01-05 22:21:40 +0900

    stockの初期設定

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -29,7 +29,8 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            StockSeeder::class,
         ]);
     }
 }
