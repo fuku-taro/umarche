27676c2e103322814cbb633140b2d758c42f6afc
fuku-taro <fukumitsu.seko@gmail.com>
2025-01-13 12:18:12 +0900

    ログイン画面の修正

--- src/resources/views/user/auth/login.blade.php
+++ src/resources/views/user/auth/login.blade.php
@@ -33,16 +33,24 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('user.password.request'))
+        <div class="flex items-center justify-between">
+        <div class="mt-4">
+            <a href="{{ route('user.register') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                新規登録
+            </a>
+        </div>
+
+        <div class="mt-4">
+            {{-- @if (Route::has('user.password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('user.password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif --}}
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+    </div>
     </form>
 </x-guest-layout>
